8daddbc7ad653e3d29e60da97accd7ba5d7ee27b
Rashidi-Moghadam <a.rashidi.m78@gmail.com>
2023-03-09 14:17:32 +0330

    added dist

--- src/pages/About.tsx
+++
@@ -1,9 +0,0 @@
-
-
-const About = () => {
-  return (
-    <div className="text-white text-3xl p-8">About</div>
-  )
-}
-
-export default About
--- src/utilites/formatCurrency.ts
+++
@@ -1,8 +0,0 @@
-const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
-  currency: "USD",
-  style: "currency",
-});
-
-export function formatCurrency(num: number) {
-  return CURRENCY_FORMATTER.format(num);
-}
